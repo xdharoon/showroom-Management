219b8f79ff1f438fc1444f8544742d10d2e0820a
xdharoon <138707276+xdharoon@users.noreply.github.com>
2024-01-11 12:13:51 +0500

    Add Servvices of Api And Testing

---
+++ ShowroomManagement/ShowroomManagementAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+
+namespace ShowroomManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        [HttpGet("GetDepartment")]  
+           public  string  GetDepartment()
+           {
+            return "Hello World";
+           }
+       
+    }
+}
--- ShowroomManagement/ShowroomManagementAPI/Program.cs
+++ ShowroomManagement/ShowroomManagementAPI/Program.cs
@@ -1,4 +1,9 @@
+using DataAcess.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_MyAllowSpecificOrigins";
+
 
 // Add services to the container.
 
@@ -6,6 +11,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(m => m.UseSqlServer("deafultConnection"));
+builder.Services.AddCors(option => option.AddPolicy(MyAllowSpecificOrigins, policy =>
+{
+    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+}));
 
 var app = builder.Build();
 
@@ -15,7 +25,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthorization();
 
 app.MapControllers();
